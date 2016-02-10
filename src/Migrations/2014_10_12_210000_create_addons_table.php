8d53ce7814c6558b87923d09b2d4f910a51f0a33
Tijmen <tijmen@floown.com>
2016-02-10 21:27:02 +0000

    Added addons table for registering add-ons with plans

---
+++ src/Migrations/2014_10_12_210000_create_addons_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddonsTable extends Migration
+{
+    /**
+     * Create the subscription table
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('subscription_id')->index()->unsigned();
+            $table->string('name');
+            $table->string('chargebee_addon_id');
+            $table->integer('quantity')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('addons');
+    }
+}
