cacfe988df8e0f16cc411d82ec3092481afddc03
Tijmen Wierenga <t.wierenga@live.nl>
2016-05-30 22:47:05 +0200

    Added add-on method and updated doc blocks

--- src/Billable.php
+++ src/Billable.php
@@ -2,8 +2,16 @@
 namespace TijmenWierenga\LaravelChargebee;
 
 
+/**
+ * Class Billable
+ * @package TijmenWierenga\LaravelChargebee
+ */
 trait Billable
 {
+    /**
+     * @param null $plan
+     * @return Subscriber
+     */
     public function subscribe($plan = null)
     {
         return new Subscriber($this, $plan);
--- src/Subscriber.php
+++ src/Subscriber.php
@@ -27,7 +27,15 @@ class Subscriber
     private $plan = null;
 
     /**
+     * An array containing all add-ons for the subscription.
      *
+     * @var array
+     */
+    private $addOns = [];
+
+    /**
+     * @param Model|null $model
+     * @param null $plan
      */
     public function __construct(Model $model = null, $plan = null)
     {
@@ -39,10 +47,54 @@ class Subscriber
         $this->model = $model;
     }
 
+    /**
+     * Create a new Chargebee subscription
+     *
+     * @throws MissingPlanException
+     */
     public function create()
     {
         if (! $this->plan) throw new MissingPlanException('No plan was set to assign to the customer.');
 
+        // TODO: Implement method
+    }
 
+    /**
+     * Convenient helper function for adding just one add-on
+     *
+     * @param $id
+     * @param $quantity
+     * @return $this
+     */
+    public function withAddOn($id, $quantity)
+    {
+        $this->addOns([
+            [
+                'id' => $id,
+                'quantity' => $quantity
+            ]
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * Adds add-ons to the subscription
+     *
+     * @param array $addOns
+     * @return $this
+     */
+    public function addOns(array $addOns)
+    {
+        foreach ($addOns as $addOn)
+        {
+            // TODO: Check if parameters are valid and catch exception.
+            $this->$addOns[] = [
+                'id'        => $addOn['id'],
+                'quantity'  => $addOn['quantity']
+            ];
+        }
+
+        return $this;
     }
 }
