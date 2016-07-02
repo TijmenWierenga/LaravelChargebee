fdb317d552e8306b3835ce760e789e2f2f0f3af5
Tijmen Wierenga <t.wierenga@live.nl>
2016-07-02 12:06:05 +0200

    Added handler for cancelled subscriptions

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -4,11 +4,31 @@ namespace TijmenWierenga\LaravelChargebee\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use TijmenWierenga\LaravelChargebee\Subscription;
 
 class WebhookController extends Controller
 {
     public function handleWebhook(Request $request)
     {
-        return response("We received your webhook but we still need to handle it. Sorry.");
+        $webhookEvent = studly_case($request->event_type);
+
+        if (method_exists($this, 'handle' . $webhookEvent)) {
+            $payload = json_decode(json_encode($request->input('content')));
+
+            return $this->{'handle' . $webhookEvent}($payload);
+        } else {
+            return response("No event handler for " . $webhookEvent, 200);
+        }
+    }
+
+    public function handleSubscriptionCancelled($payload)
+    {
+        $subscription = (new Subscription)->where('subscription_id', $payload->subscription->id)->first();
+
+        if ($subscription) {
+            // TODO: Mark subscription as cancelled instantly.
+        }
+
+        return response("Webhook handled successfully.", 200);
     }
 }
