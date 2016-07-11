0820b187d1f442159cf14a391ef5cb4e44ea0a4b
Tijmen Wierenga <t.wierenga@live.nl>
2016-07-11 10:55:39 +0200

    Removed url generation from config. Made artisan console fail

--- src/config/chargebee.php
+++ src/config/chargebee.php
@@ -4,9 +4,10 @@ return [
     // You can set the entity who gets subscribed here.
     'model' => App\User::class,
 
+    // Define your callback URI's here
     'redirect' => [
-        'success' => url('payment/success'),
-        'cancelled' => url('payment/cancelled'),
+        'success' => null,
+        'cancelled' => null,
     ],
     // Change this value to true if you want the Service Provider to create a route for handling Chargebee Webhooks
     'publish_routes' => false
